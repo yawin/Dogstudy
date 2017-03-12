5fa657585f86a2a25ae7adb2bd6ec7fe1fc560e7
yawin <yawinstake@gmail.com>
2017-03-13 00:12:00 +0800

    update study page

--- app/src/main/java/com/powerzhou/dogstudy/uimodule/study/StudyFragment.java
+++ app/src/main/java/com/powerzhou/dogstudy/uimodule/study/StudyFragment.java
@@ -1,5 +1,6 @@
 package com.powerzhou.dogstudy.uimodule.study;
 
+import android.support.annotation.Nullable;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.widget.Toolbar;
@@ -7,6 +8,7 @@ import android.support.v7.widget.Toolbar;
 import com.powerzhou.dogstudy.R;
 import com.powerzhou.dogstudy.uimodule.base.BaseFragment;
 import com.powerzhou.dogstudy.uimodule.base.IRxBusPresenter;
+import com.powerzhou.dogstudy.uimodule.widget.CustomWebView;
 
 import butterknife.BindView;
 
@@ -16,12 +18,14 @@ import butterknife.BindView;
 
 public class StudyFragment extends BaseFragment<IRxBusPresenter> {
 
+    @Nullable
     @BindView(R.id.tool_bar)
     Toolbar mToolBar;
+    @Nullable
     @BindView(R.id.tab_layout)
     TabLayout mTabLayout;
-    @BindView(R.id.view_pager)
-    ViewPager mViewPager;
+    @BindView(R.id.webview)
+    CustomWebView webview;
 
     @Override
     protected int attachLayoutRes() {
@@ -35,7 +39,11 @@ public class StudyFragment extends BaseFragment<IRxBusPresenter> {
 
     @Override
     protected void initViews() {
-        initToolBar(mToolBar, true, getString(R.string.nav_study));
+        if(mToolBar!= null) {
+            initToolBar(mToolBar, true, getString(R.string.nav_study));
+        }
+        webview.loadUrl("http://m.blog.csdn.net/article/details?id=61697599");
+
     }
 
     @Override
